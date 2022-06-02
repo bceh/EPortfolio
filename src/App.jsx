4cfed887553bcff6709a0581cdad0998e79a73e5
Francis Cheng <cbqfrancis@gmail.com>
2022-06-02 22:21:58 +1000

    Install RRD; Add Home, NavBar Components
    Install React-Router-Dom;
    Add empty Home, NavBar Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
-
+import { Route, Switch, Redirect } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
 function App() {
-  return <h1>1</h1>;
+  return (
+    <div>
+      <NavBar />
+      <div className="content">
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </div>
+    </div>
+  );
 }
 
 export default App;
